581d6e8827a3cd553cc3b6acaf23c079b1496dc5
Pratik <khadkapratikk10@gmail.com>
2025-10-27 15:52:35 +0545

    feat: add travel statistics and unique selling points data structures

---
+++ data/common/travel-stats.js
@@ -0,0 +1,18 @@
+const travelStats = [
+  {
+    label: 'of experience',
+    value: '18+ Years',
+  },
+  {
+    label: 'satisfied tourists',
+    value: '95%',
+  },
+  {
+    label: 'trip-advisor reviews',
+    value: '2950',
+  },
+  {
+    label: 'trip-advisor reviews',
+    value: '2950',
+  },
+];
---
+++ data/common/usp.js
@@ -0,0 +1,26 @@
+const usp = [
+  {
+    icon: '/public/images/common/usp/circles-three-bubbles.png',
+    title: 'Three lands, one journey',
+    description:
+      'We combine adventure, culture, and mindfulness to create trips that leave a lasting mark on your spirit.',
+  },
+  {
+    icon: '/public/images/common/usp/money.png',
+    title: 'Best price & packages',
+    description:
+      'Stay close to local traditions: spin prayer wheels in Bhutan, join Tibetan pilgrim trails, and share meals with Nepali hosts.',
+  },
+  {
+    icon: '/public/images/common/usp/pin-flag.png',
+    title: 'Guided by local experts',
+    description:
+      'Our local guides aren’t just experts in trekking — they’re storytellers, cultural bridges, and spiritual companions.',
+  },
+  {
+    icon: '/public/images/common/usp/people-like-inner-circle.png',
+    title: 'Tailored for you',
+    description:
+      'Whether you crave meditation retreats, festival immersion, or scenic adventures, we design journeys around you, not a template.',
+  },
+];
