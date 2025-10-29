c3517db1e42a2cdba02e27a40f6dcfbcdf67a6ac
Pratik <khadkapratikk10@gmail.com>
2025-10-29 11:02:12 +0545

    feat: Add staggered animation for card elements on scroll

---
+++ scripts/common/animation/stagger.js
@@ -0,0 +1,18 @@
+gsap.registerPlugin(ScrollTrigger);
+
+const cardContainerElements = document.querySelectorAll('.js-card-container');
+
+cardContainerElements.forEach((element) => {
+  const cards = element.querySelectorAll('.js-card-stagger');
+
+  gsap.from(cards, {
+    opacity: 0,
+    duration: 1,
+    stagger: 0.2,
+    ease: 'power2.out',
+    scrollTrigger: {
+      trigger: element,
+      start: 'top 80%',
+    },
+  });
+});
