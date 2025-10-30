fa307f4106f611d04bf7944a4fa636ac6c809c29
Pratik <khadkapratikk10@gmail.com>
2025-10-30 14:57:45 +0545

    feat: Wrap scroll animations in window load event listener for improved performance

--- scripts/common/animation/scroll-fade-in.js
+++ scripts/common/animation/scroll-fade-in.js
@@ -1,24 +1,26 @@
 gsap.registerPlugin(ScrollTrigger);
 
-const elements = document.querySelectorAll(`.js-scroll-fade-in`);
+window.addEventListener('load', () => {
+  const elements = document.querySelectorAll(`.js-scroll-fade-in`);
 
-elements.forEach((element) => {
-  gsap.fromTo(
-    element,
-    {
-      opacity: 0,
-      y: 100,
-    },
-    {
-      opacity: 1,
-      y: 0,
-      duration: 1,
-      ease: 'power2.out',
-      scrollTrigger: {
-        trigger: element,
-        start: 'top 80%',
-        // markers: true,
+  elements.forEach((element) => {
+    gsap.fromTo(
+      element,
+      {
+        opacity: 0,
+        y: 100,
       },
-    }
-  );
+      {
+        opacity: 1,
+        y: 0,
+        duration: 1,
+        ease: 'power2.out',
+        scrollTrigger: {
+          trigger: element,
+          start: 'top 80%',
+          // markers: true,
+        },
+      }
+    );
+  });
 });
--- scripts/common/animation/stagger.js
+++ scripts/common/animation/stagger.js
@@ -1,19 +1,21 @@
 gsap.registerPlugin(ScrollTrigger);
 
-const cardContainerElements = document.querySelectorAll('.js-card-container');
+window.addEventListener('load', () => {
+  const cardContainerElements = document.querySelectorAll('.js-card-container');
 
-cardContainerElements.forEach((element) => {
-  const cards = element.querySelectorAll('.js-card-stagger');
+  cardContainerElements.forEach((element) => {
+    const cards = element.querySelectorAll('.js-card-stagger');
 
-  gsap.from(cards, {
-    opacity: 0,
-    duration: 1,
-    delay: 0.2,
-    stagger: 0.2,
-    ease: 'power2.out',
-    scrollTrigger: {
-      trigger: element,
-      start: 'top 80%',
-    },
+    gsap.from(cards, {
+      opacity: 0,
+      duration: 1,
+      delay: 0.2,
+      stagger: 0.2,
+      ease: 'power2.out',
+      scrollTrigger: {
+        trigger: element,
+        start: 'top 80%',
+      },
+    });
   });
 });
