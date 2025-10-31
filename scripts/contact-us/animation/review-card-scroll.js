6699925c62ecafbbeb80c18d51ea9ff5421ca4ff
Pratik <khadkapratikk10@gmail.com>
2025-10-31 11:29:13 +0545

    feat: add continuous scrolling animation for review cards

---
+++ scripts/contact-us/animation/review-card-scroll.js
@@ -0,0 +1,22 @@
+const cardContainerElement = document.querySelector('.js-card-container');
+
+cardContainerElement.innerHTML += cardContainerElement.innerHTML;
+
+const totalWidth = cardContainerElement.scrollWidth / 2;
+
+const cardScroll = gsap.to(cardContainerElement, {
+  x: `-${totalWidth}`,
+  duration: 10,
+  ease: 'none',
+  repeat: -1,
+  modifiers: {
+    x: gsap.utils.unitize((x) => parseFloat(x) % totalWidth),
+  },
+});
+
+cardContainerElement.addEventListener('mouseenter', () => {
+  gsap.to(cardScroll, { timeScale: 0.2, duration: 0 });
+});
+cardContainerElement.addEventListener('mouseleave', () => {
+  gsap.to(cardScroll, { timeScale: 1, duration: 0 });
+});
