a4d5ba37d82c18dbe9cf3ddc404e3a10dd0c9b12
Pratik <khadkapratikk10@gmail.com>
2025-11-12 10:48:32 +0545

    feat: add additional contents in navbar dropdown

--- scripts/common/navbar/navbar-dropdown-panel.js
+++ scripts/common/navbar/navbar-dropdown-panel.js
@@ -1,9 +1,17 @@
 const destinationsDropdownTabElements = document.querySelectorAll(
   '.js-destinations-dropdown-tab'
 );
+const trekkingDropdownTabElements = document.querySelectorAll(
+  '.js-trekking-dropdown-tab'
+);
 const destinationsDropdownContentElements = document.querySelectorAll(
   '[class*="js-destinations-"][class$="-dropdown-content"]'
 );
+const trekkingDropdownContentElements = document.querySelectorAll(
+  '[class*="js-trekking-"][class$="-dropdown-content"]'
+);
+
+// Destinations
 destinationsDropdownTabElements.forEach((tabElement) => {
   tabElement.addEventListener('click', () => {
     destinationsDropdownTabElements.forEach((element) =>
@@ -19,6 +27,28 @@ destinationsDropdownTabElements.forEach((tabElement) => {
 
     const dropdownContentElement = document.querySelector(
       `.js-destinations-${activeTabName}-dropdown-content`
+    );
+
+    if (dropdownContentElement) dropdownContentElement.classList.add('active');
+  });
+});
+
+// Trekking
+trekkingDropdownTabElements.forEach((tabElement) => {
+  tabElement.addEventListener('click', () => {
+    trekkingDropdownTabElements.forEach((element) =>
+      element.classList.remove('active')
+    );
+    trekkingDropdownContentElements.forEach((element) =>
+      element.classList.remove('active')
+    );
+
+    tabElement.classList.add('active');
+
+    const activeTabName = tabElement.textContent.toLowerCase().trim();
+
+    const dropdownContentElement = document.querySelector(
+      `.js-trekking-${activeTabName}-dropdown-content`
     );
 
     if (dropdownContentElement) dropdownContentElement.classList.add('active');
